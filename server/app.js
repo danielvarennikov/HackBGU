a433326aa13b8cfd2081a232b26c4f1b21718014
andalusm <37974151+andalusm@users.noreply.github.com>
2020-09-13 21:48:19 +0300

    Added mongoose and ejs

--- server/app.js
+++ server/app.js
@@ -1,47 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect("mongodb://localhost:27017/studentDB",{ useUnifiedTopology: true ,useNewUrlParser: true});
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const studentSchema = new mongoose.Schema({
+   name: String,
+   score: Number
+});
+const Student = mongoose.model('Student', studentSchema);
+const app = express();
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/ranking", function(req, res) {
+  Student.find({},function(err,students){
+students.sort(function(a,b){
+  return b.score-a.score;
+})
+    res.render("ranking",{students:students,name:"Baraa"});
+});
 });
 
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
+
+
+app.get("/video/:postID", function(req, res){
+  const postID = req.params.postID;
+  Post.findById(postID, function (err, post) {
+    if(err || !post)
+    {
+      console.log("No post was found");
+    }
+    else{
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
 })
 
-module.exports = app;
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started successfully");
+});
